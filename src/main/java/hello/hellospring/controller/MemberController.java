375e84dfd7df58be938a1cade613785df4f732fb
GoBoKyung <113565954+GoBoKyung@users.noreply.github.com>
2022-11-08 02:07:22 +0900

    InQ3

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,12 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.lang.reflect.Member;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,4 +18,25 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+    @GetMapping{"/members/new"}
+    public String createForm(){
+        return "member/createMemberForm";
+
+    }
+    @PostMapping("/members/new")
+    public String create(Memberform form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect;/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> mambers = memberService.findMembers();
+        model.addAttribute( attributeName: "members", members);
+        return "members/memberList";
+    }
+
 }
