eaa6702dc8521aa04cc9aefdedc6a178951037b1
GoBoKyung <113565954+GoBoKyung@users.noreply.github.com>
2022-11-21 22:25:21 +0900

    InQ4

--- src/main/java/hello/hellospring/domain/member.java
+++ src/main/java/hello/hellospring/domain/member.java
@@ -1,6 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
 
     private Long id;
     private String name;
